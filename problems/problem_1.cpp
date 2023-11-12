701772973198a7504d14c07587aa0fc612c14467
Gulyuz <g.ibragimova@newuu.uz>
2023-11-12 22:49:21 +0500

    problem 1

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -2,5 +2,21 @@ void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
 
+    const float fixedAmount = 13;
+    const float rate1 = 0.4;
+    const float rate2 = 0.12;
+    const float rate3 = 1.4;
+    const float additionalRate = 1.5;
+
+    if (consumed_water <= 30) {
+        cost = fixedAmount + consumed_water * rate1;
+    } else if (consumed_water <= 50) {
+        cost = fixedAmount + 30 * rate1 + (consumed_water - 30) * rate2;
+    } else if (consumed_water <= 60) {
+        cost = fixedAmount + 30 * rate1 + 20 * rate2 + (consumed_water - 50) * rate3;
+    } else {
+        cost = fixedAmount + 30 * rate1 + 20 * rate2 + 10 * rate3 + (consumed_water - 60) * additionalRate;
+    }
+
     return cost;
 }
