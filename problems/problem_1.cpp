d806049c55d40a7c1292a637e2f8f5db7048915b
Gulyuz <g.ibragimova@newuu.uz>
2023-11-12 20:56:59 +0500

    problem 1,2

--- problems/problem_1.cpp
+++ problems/problem_1.cpp
@@ -1,8 +1,6 @@
 void problemSolution1(float consumed_water) {
     float cost;
     // write your code here
-    const float rate_per_unit = 0.05;
-    float cost = consumed_water * rate_per_unit;
 
     return cost;
 }
