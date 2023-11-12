8999c38b33518cbacb18b916990c99c8308d16c6
Gulyuz <g.ibragimova@newuu.uz>
2023-11-12 21:47:57 +0500

    problem 3

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -2,7 +2,25 @@
 
 std::string problemSolution3(float height, char S) {
     // write your code here
-
+    if (S == 'M') {
+        if (height < 1.7) {
+            heightDescription = "Short";
+        } else if (height >= 1.7 && height < 1.85) {
+            heightDescription = "Normal";
+        } else {
+            heightDescription = "Tall";
+        }
+    } else if (S == 'F') {
+        if (height < 1.6) {
+            heightDescription = "Short";
+        } else if (height >= 1.6 && height < 1.75) {
+            heightDescription = "Normal";
+        } else {
+            heightDescription = "Tall";
+        }
+    } else {
+        heightDescription = "Invalid sex";
+    }
     // use return to return your result
     // make use of control flow statements
 }
